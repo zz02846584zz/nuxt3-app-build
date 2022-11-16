27feb97c26d42cde9bb508c28ff1c3d32ecd0575
brian chang <zz02846584zz@gmail.com>
2022-11-16 19:55:32 +0800

    first commit

---
+++ server/chunks/app/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-dbf68403.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.bae93bcf.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
